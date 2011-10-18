83cd1b3f67e305c6849b680308094433ef647e01
Stefano Palazzo <stefano.palazzo@gmail.com>
2011-10-18 12:30:07 +0200

    improved help(), added copyright notice (ISC license)

--- pbkdf2.py
+++ pbkdf2.py
@@ -1,3 +1,18 @@
+''' Password based key-derivation function - PBKDF2 '''
+
+# Copyright (c) 2011, Stefano Palazzo <stefano.palazzo@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import hmac
 import hashlib
@@ -7,8 +22,6 @@ import struct
 
 def pbkdf2(digestmod, password: 'bytes', salt, count, dk_length) -> 'bytes':
     '''
-    Password based key-derivation function - PBKDF2
-
     PBKDF2, from PKCS #5 v2.0:
         http://tools.ietf.org/html/rfc2898
 
